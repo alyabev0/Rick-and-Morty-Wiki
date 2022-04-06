bba43ed3266853dd6662c9b6b7ce1d26b714ae23
Pavel <alyabev1998@gmail.com>
2022-04-07 01:36:28 +0300

    safe commit

--- src/App.js
+++ src/App.js
@@ -10,11 +10,6 @@ import Navbar from "./components/Navbar/Navbar";
 
 
 
-
-
-
-
-
 function App() {
 
   let [fetchedData, updateFetchedData] = useState([]);
@@ -23,7 +18,12 @@ function App() {
   let [pageNumber, updatePageNumber] = useState(1);
   let [search, setSearch] = useState("");
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
+
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
   useEffect(() => {
     (async function () {
@@ -42,7 +42,15 @@ function App() {
 
   <div className="container">
   <div className="row">
-    Filter component will be placed here
+  <Filter
+  pageNumber={pageNumber}
+  status={status}
+  updateStatus={updateStatus}
+  updateGender={updateGender}
+  updateSpecies={updateSpecies}
+  updatePageNumber={updatePageNumber}
+/>
+
     <div className="col-lg-8 col-12">
       <div className="row">
         <Card results = {results} />
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Gender from "./category/Gender";
+import Species from "./category/Species";
+import Status from "./category/Status";
