8742d675e239c9db2c931b3231840e55c74e11ce
Pavel <alyabev1998@gmail.com>
2022-04-06 20:11:40 +0300

    added styles to character cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
-import Card from './components/Card/Card';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+
 
 var api = `https://rickandmortyapi.com/api/character/?page=1`
 
