2491295751fa620f0e5752f7bb4b944823869705
Pavel <alyabev1998@gmail.com>
2022-04-07 19:03:28 +0300

    added react router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Title from "./components/Title/Title"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Title />
+    {/* <Title /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
